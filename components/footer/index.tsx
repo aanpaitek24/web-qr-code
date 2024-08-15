a310263cfd3e9594e0d1a165a1e8bb3b952857c6
aanpaitek24 <aanp@aitek.site>
2024-08-15 10:10:26 +0800

    Update index.tsx

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,59 +1,59 @@
 import {
-  ButtonGroup,
-  Container,
-  IconButton,
-  Stack,
-  Text,
-  Link,
-  useMediaQuery,
-} from "@chakra-ui/react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { BiDonateHeart } from "react-icons/bi";
-
-const Footer = () => {
-  const [isMobile] = useMediaQuery("(max-width: 1280px)");
-  return (
-    <>
-      <hr />
-      <Container as="footer" role="contentinfo" py={{ base: "2", md: "2" }}>
-        <Stack spacing={{ base: isMobile ? "0" : "-10", md: "-10" }}>
-          <Stack justify="space-between" direction="column" align="flex-end">
-            <ButtonGroup variant="ghost">
-              <IconButton
-                as="a"
-                href="https://www.linkedin.com/in/dante-lentsoe-2b6566148/"
-                aria-label="LinkedIn"
-                icon={<FaLinkedin fontSize="1.25rem" />}
-              />
-              <IconButton
-                as="a"
-                href="https://github.com/dantelentsoe"
-                aria-label="GitHub"
-                icon={<FaGithub fontSize="1.25rem" />}
-              />
-
-              <IconButton
-                as="a"
-                href="https://www.buymeacoffee.com/dantelentsoe "
-                aria-label="BuyMeACoffe"
-                icon={<BiDonateHeart fontSize="1.25rem" />}
-              />
-            </ButtonGroup>
+    ButtonGroup,
+    Container,
+    IconButton,
+    Stack,
+    Text,
+    Link,
+    useMediaQuery,
+  } from "@chakra-ui/react";
+  import { FaGithub, FaLinkedin } from "react-icons/fa";
+  import { BiDonateHeart } from "react-icons/bi";
+  
+  const Footer = () => {
+    const [isMobile] = useMediaQuery("(max-width: 1280px)");
+    return (
+      <>
+        <hr />
+        <Container as="footer" role="contentinfo" py={{ base: "2", md: "2" }}>
+          <Stack spacing={{ base: isMobile ? "0" : "-10", md: "-10" }}>
+            <Stack justify="space-between" direction="column" align="flex-end">
+              <ButtonGroup variant="ghost">
+                <IconButton
+                  as="a"
+                  href=""
+                  aria-label="LinkedIn"
+                  icon={<FaLinkedin fontSize="1.25rem" />}
+                />
+                <IconButton
+                  as="a"
+                  href=""
+                  aria-label="GitHub"
+                  icon={<FaGithub fontSize="1.25rem" />}
+                />
+  
+                <IconButton
+                  as="a"
+                  href=""
+                  aria-label=""
+                  icon={<BiDonateHeart fontSize="1.25rem" />}
+                />
+              </ButtonGroup>
+            </Stack>
+  
+            <Text fontSize="sm" color="subtle" py={{ base: "2", md: "2" }}>
+              &copy; {new Date().getFullYear()} Developed by{" "}
+              <Link
+                href=""
+                target="_blank"
+                color={"teal.500"}>
+                Emil Alvaro
+              </Link>
+            </Text>
           </Stack>
-
-          <Text fontSize="sm" color="subtle" py={{ base: "2", md: "2" }}>
-            &copy; {new Date().getFullYear()} Developed by{" "}
-            <Link
-              href="https://dantelentsoe.com"
-              target="_blank"
-              color={"teal.500"}>
-              Dante Lentsoe
-            </Link>
-          </Text>
-        </Stack>
-      </Container>
-    </>
-  );
-};
-
-export default Footer;
+        </Container>
+      </>
+    );
+  };
+  
+  export default Footer;
